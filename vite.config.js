3502d21afae7deddf8d3ac41d37bcd5b9edc69c2
Jorge Duran <jorgeluis21.duran@gmail.com>
2024-11-08 07:51:37 -0400

    fix: :bug: Chaging vite config for /callback reditect to work fine

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { BACKEND_URI } from './src/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      '/callback': {
+        target: BACKEND_URI, // Replace with your actual OAuth provider URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/callback/, '')
+      },
       '/yt-music': {
         target: 'https://music.youtube.com',
         changeOrigin: true,
